4329c65400bf078a5077ab01fb882c686832092a
quoctrung163 <quoctrung163@yandex.com>
2020-02-16 22:37:26 +0700

    add

---
+++ main.js
@@ -0,0 +1,14 @@
+function getFunc(getNumberFunc) {
+    return arr => {
+        return getNumberFunc(arr);
+    }
+}
+
+function getNumberFunc(arr) {
+    return Math.max(...arr);
+    // Ex: Math.max(1, 2, 3) => 3
+}
+
+const getNumber = getFunc(getNumberFunc);
+
+console.log(getNumber([1, -5, 8999]));
